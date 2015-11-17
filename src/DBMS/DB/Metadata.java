87628ae2266452f53a4c016bf817c2c643b87b21
Dmitry Kapitun <KapChook@users.noreply.github.com>
2015-11-18 01:31:37 +0300

    Added methods for Metadata

--- src/DBMS/DB/Metadata.java
+++ src/DBMS/DB/Metadata.java
@@ -2,14 +2,47 @@ package DBMS.DB;
 
 import DBMS.Argument;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeSet;
 
 /**
  * Created by dmitriy on 11/17/2015.
  */
 public class Metadata {
-    Map<String, ArrayList<Argument>> tableStructure;
-    Map<String, ArrayList<Integer>> tablePages;
-    Map<String, ArrayList<Integer>> indexPages;
+    Map<String, TreeSet<Argument>> tableStructure;
+    Map<String, IndexType> indexType;
+    Map<String, TreeSet<Integer>> tabIndPages;
+
+    enum IndexType {
+        Tree, Hash
+    }
+
+    public Metadata() {
+        tableStructure = new HashMap<>();
+        tabIndPages = new HashMap<>();
+        indexType = new HashMap<>();
+    }
+
+    public void addArgument(String table, Argument argument) {
+        if (!tableStructure.containsKey(table)) tableStructure.put(table, new TreeSet<>());
+
+        tableStructure.get(table).add(argument);
+    }
+
+    public void addPage(String to, int page) {
+        if (!tabIndPages.containsKey(to)) tableStructure.put(to, new TreeSet<>());
+
+        tabIndPages.get(to).add(page);
+    }
+
+    public void addIndexType(String index, IndexType type) {
+        indexType.put(index, type);
+    }
+
+    public void removePage(String from, int page) {
+        if (!tabIndPages.containsKey(from)) return;
+
+        tabIndPages.get(from).remove(page);
+    }
 }
