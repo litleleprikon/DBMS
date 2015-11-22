e0e408d96fa678a007e61af475a51aec05d007d9
Emil Sharifullin <litleleprikon@gmail.com>
2015-11-22 04:02:23 +0300

    Added implementation of Iterator class to Table

--- src/DBMS/DB/InnerStructure/Table.java
+++ src/DBMS/DB/InnerStructure/Table.java
@@ -4,21 +4,24 @@ import java.util.*;
 
 /**
  * Created by dmitriy on 20-Nov-15.
+ * Class for tables
  */
-public class Table {
+public class Table implements Iterator<Tuple>{
     private String name;
     private Map<String, Argument> arguments;
-    private Set<Tuple> tuples;
+    private LinkedList<Tuple> tuples;
     private LinkedList<Argument> primaryKeys;
     private LinkedList<Argument> foreignKeys;
     private LinkedList<Index> indexes;
+    private int counter;
 
     public Table(String name) {
         this.name = name;
         arguments = new LinkedHashMap<>();
-        tuples = new HashSet<>();
+        tuples = new LinkedList<>();
         foreignKeys = new LinkedList<>();
         primaryKeys = new LinkedList<>();
+        counter = 0;
     }
 
     public void addArgument(Argument argument) {
@@ -33,7 +36,17 @@ public class Table {
         return arguments;
     }
 
-    public Set<Tuple> getTuples() {
+    public LinkedList<Tuple> getTuples() {
         return tuples;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return counter < tuples.size()-1;
+    }
+
+    @Override
+    public Tuple next() {
+        return tuples.get(counter++);
     }
 }
