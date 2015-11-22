f556553921fcd2afb0e993141713592edc18a169
Emil Sharifullin <litleleprikon@gmail.com>
2015-11-22 14:32:09 +0300

    Created Indexes

--- src/DBMS/DB/InnerStructure/Indexes/HashIndex.java
+++ src/DBMS/DB/InnerStructure/Indexes/HashIndex.java
@@ -1,7 +1,19 @@
 package DBMS.DB.InnerStructure.Indexes;
 
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by dmitriy on 22-Nov-15.
  */
-public class HashIndex extends Index {
+public class HashIndex<K extends Comparable<K>, V> extends Index {
+    public HashIndex(String name, LinkedList linkedList) {
+        super(name, linkedList);
+    }
+
+    @Override
+    public Set<Entry> entrySet() {
+        return null;
+    }
 }
--- src/DBMS/DB/InnerStructure/Indexes/TreeIndex.java
+++
@@ -1,7 +0,0 @@
-package DBMS.DB.InnerStructure.Indexes;
-
-/**
- * Created by dmitriy on 22-Nov-15.
- */
-public class TreeIndex extends Index {
-}
